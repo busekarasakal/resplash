41848a5ea6722f0e2debb478d43ade9992464d2f
Buse Karasakal <buseozvtn@gmail.com>
2023-04-14 22:54:06 +0200

    fix: header scaling issues

--- src/components/SubHeader/index.tsx
+++ src/components/SubHeader/index.tsx
@@ -12,19 +12,25 @@ export function SubHeader(props: PropsWithChildren<{ title: string }>) {
     <Stack
       direction={isLargeScreenDevice ? 'row' : 'column'}
       alignItems='center'
+      justifyContent='space-between'
       gap={1}
     >
-      <Box width='100%'>
+      <Box
+        overflow={'hidden'}
+        flex={1}
+        maxWidth={isLargeScreenDevice ? '40%' : '100%'}
+      >
         <Typography
           variant='h4'
           fontWeight='bold'
           textAlign={isLargeScreenDevice ? 'left' : 'center'}
           noWrap
         >
+          sdgsadgasgdasgasdgasdgasdgsgasgdasdgds
           {props.title}
         </Typography>
       </Box>
-      {props.children}
+      <Box justifyContent={'flex-end'}>{props.children}</Box>
     </Stack>
   );
 }
